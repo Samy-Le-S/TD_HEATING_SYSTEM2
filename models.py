af0f4e8392e056a6e0b6d40c9c2bc6082fca588c
Samy Benaissa <samy77680@gmail.com>
2026-05-11 10:19:58 +0200

    ajout des zones

--- models.py
+++ models.py
@@ -46,4 +46,9 @@ class Installation:
                 
         return True
     
-    
+
+class Zone:
+    def __init__(self, nom: str, surface_m2: float, equipement_associe: Equipement):
+        self.nom = nom
+        self.surface_m2 = surface_m2
+        self.equipement_associe = equipement_associe
